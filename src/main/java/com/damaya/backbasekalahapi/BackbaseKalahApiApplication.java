309bafa0ece9c73297c81cda8b6e8ba347dfd476
Diego Amaya <damaya0226@gmail.com>
2020-01-28 12:34:42 +0100

    first commit

---
+++ src/main/java/com/damaya/backbasekalahapi/BackbaseKalahApiApplication.java
@@ -0,0 +1,13 @@
+package com.damaya.backbasekalahapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackbaseKalahApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackbaseKalahApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/damaya/backbasekalahapi/BackbaseKalahApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damaya.backbasekalahapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackbaseKalahApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
