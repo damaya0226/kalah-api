ff25913231ce8c76f1cf7a1814dad90b381853d7
Diego Amaya <damaya0226@gmail.com>
2020-01-30 11:13:19 +0100

    Sonar fixes

--- rest-api/src/main/java/com/damaya/kalah/advices/RestExceptionHandler.java
+++ rest-api/src/main/java/com/damaya/kalah/advices/RestExceptionHandler.java
@@ -5,12 +5,9 @@ import com.damaya.kalah.core.entities.exceptions.GameNotFoundException;
 import com.damaya.kalah.core.entities.exceptions.InvalidMoveException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletResponse;
@@ -19,23 +16,23 @@ import java.io.IOException;
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(RestExceptionHandler.class);
+    private static final Logger LOCAL_LOGGER = LoggerFactory.getLogger(RestExceptionHandler.class);
 
     @ExceptionHandler({GameNotFoundException.class})
     public void handleGameNotFoundException(GameNotFoundException e, HttpServletResponse response) throws IOException {
-        LOGGER.debug(e.getMessage());
+        LOCAL_LOGGER.debug(e.getMessage());
         response.sendError(HttpStatus.NOT_FOUND.value());
     }
 
     @ExceptionHandler({InvalidMoveException.class})
     public void handleInvalidMoveException(InvalidMoveException e, HttpServletResponse response) throws IOException {
-        LOGGER.debug(e.getMessage());
+        LOCAL_LOGGER.debug(e.getMessage());
         response.sendError(HttpStatus.BAD_REQUEST.value());
     }
 
     @ExceptionHandler({GameAlreadyFinishedException.class})
     public void handleInvalidMoveException(GameAlreadyFinishedException e, HttpServletResponse response) throws IOException {
-        LOGGER.debug(e.getMessage());
+        LOCAL_LOGGER.debug(e.getMessage());
         response.sendError(HttpStatus.CONFLICT.value());
     }
 }
